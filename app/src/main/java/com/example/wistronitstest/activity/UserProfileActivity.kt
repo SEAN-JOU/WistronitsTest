092e93d76255a2dbe1c028da3680134c00d23109
sean\jou <sean.chou@jamzoo.com.tw>
2021-04-05 15:16:16 +0800

    個人資料UI完成

--- app/src/main/java/com/example/wistronitstest/activity/UserProfileActivity.kt
+++ app/src/main/java/com/example/wistronitstest/activity/UserProfileActivity.kt
@@ -2,14 +2,11 @@ package com.example.wistronitstest.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import com.example.wistronitstest.R
-import com.example.wistronitstest.api.ApiManager
+import com.example.wistronitstest.bean.UserProfile
 import com.example.wistronitstest.bean.UserProfileData
 import com.example.wistronitstest.model.UserProfileModel
-import com.example.wistronitstest.model.UsersListModel
 import com.example.wistronitstest.presenter.UserProfilePresenter
-import com.example.wistronitstest.presenter.UsersListPresenter
 import com.example.wistronitstest.view.UserProfileView
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_user_profile.*
@@ -30,9 +27,14 @@ class UserProfileActivity : AppCompatActivity(),UserProfileView {
     }
 
     override fun showUserProfileView(info: UserProfileData) {
+        var userProfile : UserProfile = info.data
         runOnUiThread {
-            Picasso.with(this).load(info.data.avatar_url).placeholder(R.mipmap.ic_launcher_round)
+            Picasso.with(this).load(userProfile.avatar_url).placeholder(R.mipmap.ic_launcher_round)
                 .into(headImageView)
+            nameTextView.text = userProfile.name
+            loginTextView.text = userProfile.login
+            locationTextView.text = userProfile.location
+            linkTextView.text = userProfile.blog
         }
     }
 }
